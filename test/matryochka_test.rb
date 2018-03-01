2dba95700eadd850bc6e05fb4e1f5ecb9b196189
Jérémie Bonal <jeremie.bonal@gmail.com>
2018-03-01 21:33:55 +0100

    Added a test to check on nil-return when out of stack.

--- test/matryochka_test.rb
+++ test/matryochka_test.rb
@@ -3,13 +3,21 @@ require_relative '../lib/matryochka.rb'
 
 class MatryochkaTest < Minitest::Test
   def test_method_can_get_the_binding_of_their_parent
+    test = :hello
     fetched_binding = get_binding
-    assert_equal fetched_binding, binding
+    assert_equal fetched_binding.local_variable_get(:test),
+                 binding.local_variable_get(:test)
   end
 
   def test_a_method_can_get_the_binding_of_higher_up_ancestors
+    test = :hello
     fetched_binding = parent_method(levels: 2)
-    assert_equal fetched_binding, binding
+    assert_equal fetched_binding.local_variable_get(:test),
+                 binding.local_variable_get(:test)
+  end
+
+  def test_returning_nil_if_frame_doesnt_exist
+    assert_nil Matryochka.get_binding(up: 5_000_000) # Sure, you never know, but I feel pretty safe
   end
 
   private
@@ -19,6 +27,7 @@ class MatryochkaTest < Minitest::Test
   end
 
   def parent_method(*args, **kwargs)
+    test = :bonjour
     get_binding(*args, **kwargs)
   end
 end
