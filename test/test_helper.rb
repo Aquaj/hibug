d09d60f123d1d02b7eff1c52d60b29c38cd67638
Jérémie Bonal <jeremie.bonal@gmail.com>
2018-03-09 22:37:40 +0100

    Added a way to stub stdin

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,1 +1,28 @@
 require 'minitest/autorun'
+require 'timeout'
+
+module TestHelper
+  def stub_stdin(*inputs)
+    stub = StringIO.new
+    inputs.each { |input| stub.puts(input.to_s) }
+    stub.rewind
+    stub
+  end
+
+  def run_with_stdin(std_stub)
+    original = $stdin
+    $stdin = std_stub
+    Timeout.timeout(1) do
+      yield
+    end
+  rescue Timeout::Error
+    # noop
+  ensure
+    $stdin = original
+  end
+end
+
+class Minitest::Test
+  include TestHelper
+end
+
