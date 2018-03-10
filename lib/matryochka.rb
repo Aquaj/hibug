6f86b801750e8e452a07c61b12dfc59631017cb0
Jérémie Bonal <jeremie.bonal@gmail.com>
2018-03-10 01:37:45 +0100

    Added binding to the current context.

--- lib/matryochka.rb
+++ lib/matryochka.rb
@@ -4,10 +4,18 @@ Matryochka = Object.new
 
 class << Matryochka
   def get_binding(up: 1)
+    current_info(up: up)&.binding
+  end
+
+  def current_info(up: 1)
     RubyVM::DebugInspector.open do |dc|
-      frame_position = up + 2 # 1 in DebugInspector#open + 1 in #get_binding
+      frame_position = up + 3 # 1 in DebugInspector#open + 1 in #get_binding + 1 in #current_info
       return nil if frame_position >= dc.backtrace_locations.size
-      dc.frame_binding(frame_position)
+      Struct.new(:locations, :binding, :iseq, :klass)
+            .new(dc.backtrace_locations,
+                 dc.frame_binding(frame_position),
+                 dc.frame_iseq(frame_position),
+                 dc.frame_class(frame_position))
     end
   end
 
--- lib/repl.rb
+++ lib/repl.rb
@@ -1,8 +1,23 @@
+require_relative 'matryochka'
+
 module REPL
   def repl_launch
     loop do
       code = gets
-      puts eval(code).inspect if code
+      puts current_binding.eval(code).inspect if code
+    end
+  end
+
+  private
+
+  def current_binding
+    currently_in_repl = nil
+    (1...Matryochka.current_info.locations.size).each do |current|
+      prev_in_repl = currently_in_repl
+      context = Matryochka.current_info(up: current)
+      current_label = context&.iseq&.label
+      currently_in_repl = (current_label == 'repl_launch')
+      break context.binding if prev_in_repl && !currently_in_repl
     end
   end
 end
